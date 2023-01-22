9610da905b4c8284a02b83597a59eef7d22b9100
johnmuthua <caveomuthama@gmail.com>
2023-01-22 12:12:26 +0300

    added the rand module

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     let mut guess = String::new();
-    println!("Guess the secret number");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("Enter your number");
     io::stdin().read_line(&mut guess).expect("Wrong number");
     println!("Your guess is {}", guess);
+    println!("The secret Number is {}", secret_number);
 }
