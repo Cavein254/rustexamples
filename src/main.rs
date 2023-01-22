e3aadea055ae61337f8f64dd80788288887c9ab1
johnmuthua <caveomuthama@gmail.com>
2023-01-22 11:47:56 +0300

    added second functionality

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
 use std::io;
+
 fn main() {
-    println!("Guesing Game:::");
-    println!("Enter the input number->");
-
     let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Unable to read line");
-
-    println!("You Guessed {}", guess);
+    println!("Guess the secret number");
+    io::stdin().read_line(&mut guess).expect("Wrong number");
+    println!("Your guess is {}", guess);
 }
