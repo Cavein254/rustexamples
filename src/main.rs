d04f44c0c5a8be72120a05bc23fddb2a76d8fdc2
johnmuthua <caveomuthama@gmail.com>
2023-01-25 10:58:31 +0300

    scope within functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+fn add_suffix(mut s: String) {
+    s.push_str(" world");
+    println!("{}", { s });
+}
 fn main() {
-    let s1 = String::from("Hello");
-    let s2 = s1;
-
-    // println!("{}, world!", s1); //does not compile
-    println!("{s2}, world!");
+    let s = String::from("Hello");
+    add_suffix(s);
+    // println!("{}", { s }); //Here does not compile as s moves out of scope
 }
