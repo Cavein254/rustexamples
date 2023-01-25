d06f535b03e9a6ed719188e683a5911289aab398
johnmuthua <caveomuthama@gmail.com>
2023-01-25 11:09:33 +0300

    second version of borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-fn add_suffix(mut s: String) {
-    s.push_str(" world");
-    println!("{}", { s });
+fn add_suffix(s: String) {
+    let mut p = String::from(" world!");
+    p.push_str(&s);
+    println!("{}", { p });
 }
 fn main() {
     let s = String::from("Hello");
